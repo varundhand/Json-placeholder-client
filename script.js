ed560497394b197c48a9935af6836e5aff2d3620
varundhand <varundhand77@gmail.com>
2023-04-02 15:00:01 +0530

    fixed sort not working in api call

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ const goBtnElement = document.querySelector("button");
 let resource;
 let id;
 let page;
-let sort;
+let sort = null;
+// const sort = sort && `&_order=${sort}`;
 
 function resourceClick(evt) {
   resource = evt.target.value;
@@ -14,6 +15,7 @@ function resourceClick(evt) {
 
 function sortClick(evt) {
   sort = evt.target.value;
+  // sort = sort && `&_order=${sort}`;
 }
 
 goBtnElement.addEventListener("click", goBtnFunction);
@@ -23,13 +25,21 @@ async function goBtnFunction() {
   const idDataCheck = idData && `?id=${idData}`;
   const pageData = pageElement.value;
   const pageDataCheck = pageData && `&_page=${pageData}`;
+  function checkSort(sortValue) {
+    if (sortValue) {
+      return `&_order=${sortValue}`;
+    } else {
+      return "";
+    }
+  }
 
   //! also check whether the variables exist or not before injecting them to final api call
   const finalCall = await fetch(
-    `https://jsonplaceholder.typicode.com/${resource}${idDataCheck}${pageDataCheck}`
+    `https://jsonplaceholder.typicode.com/${resource}${idDataCheck}${pageDataCheck}${checkSort(
+      sort
+    )}`
   );
   const data = await finalCall.json();
-  console.log(data);
-
-  console.log(finalCall);
+  const dataObj = data[0];
+  const { id, title, userId, body } = dataObj; // body gets back in some resource paths only
 }
