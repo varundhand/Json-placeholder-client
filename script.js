ee3c59d9fbd34f10087358813be185a87d7a6705
varundhand <varundhand77@gmail.com>
2023-04-02 23:10:34 +0530

    GET method working and did some styling

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const idElement = document.querySelector("#id"); // fetch it through GO button function
 const pageElement = document.querySelector("#page");
 const goBtnElement = document.querySelector("button");
+const responseElement = document.querySelector(".response-box");
 
 // variables to be used in endpoint
 let resource = "users";
@@ -24,38 +25,38 @@ goBtnElement.addEventListener("click", goBtnFunction);
 async function goBtnFunction() {
   let idData;
   let pageData;
+  // console.log(pageElement.value);
   if (idElement.value > 10 || idElement.value < 0) {
     window.alert("Enter ID Value between 1-10");
     return;
-  } else if (pageElement.value < 1) {
-    window.alert("Enter Page No. more than 1");
-    return;
+    // } else if (pageElement.value < 1) { //TODO: page not working
+    //   window.alert("Enter Page No. more than 0");
+    //   return;
   }
   idData = idElement.value;
   pageData = pageElement.value;
   const idDataCheck = idData && `id=${idData}`; // this is shorter version of ternary condition
   const pageDataCheck = pageData && `&_page=${pageData}`;
-  // function checkSort(sortValue) {
-  //   if (sortValue) {
-  //     return `&_order=${sortValue}`;
-  //   } else {
-  //     return "";
-  //   }
-  // }
 
   //! also check whether the variables exist or not before injecting them to final api call
   const finalCall = await fetch(
     `https://jsonplaceholder.typicode.com/${resource}?${idDataCheck}${pageDataCheck}${sort}`
   );
   const data = await finalCall.json();
-  const dataObj = data[0];
-  console.log(finalCall);
-  console.log(data);
-  // const { id, title, userId, body } = dataObj; //! body gets back in some resource paths only
+  const stringifiedData = JSON.stringify(data, null, 2);
+  console.log({ stringifiedData });
 
-  // showResponse(finalData);
+  // data.forEach((singleObj) => {
+  //   for (const key2 in singleObj) {
+  //     if (singleObj.hasOwnProperty(key2)) {
+  //       console.log(`${key2} : ${singleObj[key2]}`);
+  //     }
+  //   }
+  // });
+  showResponse(stringifiedData);
 }
 
-// function showResponse(response){
-
-// }
+function showResponse(object) {
+  console.log(responseElement);
+  responseElement.value = object;
+}
